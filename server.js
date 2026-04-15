48431dfb106145158cec10a5a9ef835a942ad51c
Kushagra Tiwari <kushagratiwari86@gmail.com>
2026-04-16 01:05:56 +0530

    check 2

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(cors({
     // In production or if CORS_ORIGIN is set, use specific origins
     const allowedOrigins = process.env.CORS_ORIGIN 
       ? process.env.CORS_ORIGIN.split(',').map(o => o.trim())
-      : ['http://localhost:5173', 'http://localhost:5174'];
+      : ['http://localhost:5173', 'http://localhost:5174','https://capstone-kfwu.onrender.com/'];
     
     if (allowedOrigins.indexOf(origin) !== -1) {
       return callback(null, true);
